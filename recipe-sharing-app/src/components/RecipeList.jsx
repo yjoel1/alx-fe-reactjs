ed30beb913f461ebdd5431afa752745b6994dd26
yjoel1 <joelyeddu@gmail.com>
2026-01-28 09:46:20 -0800

    Update

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,20 @@
+import { useRecipeStore } from './recipeStore';
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      <h2>Recipes</h2>
+      {recipes.length === 0 && <p>No recipes added yet.</p>}
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
