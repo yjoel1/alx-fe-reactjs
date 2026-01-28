730a97a8690eee3cfdbc3a9c077a954184a5c29c
yjoel1 <joelyeddu@gmail.com>
2026-01-28 06:25:07 -0800

    Update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,8 +1,8 @@
 
 import WelcomeMessage from './components/WelcomeMessage';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
