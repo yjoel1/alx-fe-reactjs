2bb141f2a6fd4882421713d4d235dc79c3e737b8
yjoel1 <joelyeddu@gmail.com>
2026-01-28 06:08:38 -0800

    Update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -36,3 +36,18 @@ function App() {
 }
 
 export default App
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
