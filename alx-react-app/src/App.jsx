39089ed2e436d43f04a33ab22cdaa0148e8e5209
yjoel1 <joelyeddu@gmail.com>
2026-01-28 06:36:02 -0800

    Update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,18 +1,26 @@
-
+import UserProfile from './components/UserProfile';
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
+
 function App() {
   return (
     <div>
-      <Header />
+        <Header />
       <MainContent />
       <Footer />
-       <WelcomeMessage />
+      < WelcomeMessage />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
     </div>
   );
 }
 
+
 export default App;
+
