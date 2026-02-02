83cfa39418d5e6edfce89f2fe9df3295dbf980eb
yjoel1 <joelyeddu@gmail.com>
2026-02-02 07:37:38 -0800

    Update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,20 @@
-export const fetchUserData = async (username) => {
+import axios from 'axios';
+
+export const fetchAdvancedUsers = async (
+  username,
+  location,
+  minRepos,
+  page = 1
+) => {
+  let query = '';
+
+  if (username) query += `${username} `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos}`;
+
   const response = await axios.get(
-    `https://api.github.com/users/${username}`
+    `https://api.github.com/search/users?q=${query.trim()}&page=${page}&per_page=10`
   );
+
   return response.data;
 };
