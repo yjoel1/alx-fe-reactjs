92d710a3cfa050a27883424f1471c68cbf976069
yjoel1 <joelyeddu@gmail.com>
2026-02-02 07:04:07 -0800

    Update

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,16 +4,17 @@ function Navbar() {
   return (
     <nav
       style={{
+        padding: '15px',
         backgroundColor: '#333',
-        padding: '10px',
         display: 'flex',
-        gap: '15px'
+        justifyContent: 'space-around',
+        alignItems: 'center'
       }}
     >
-      <Link style={{ color: 'white', textDecoration: 'none' }} to="/">Home</Link>
-      <Link style={{ color: 'white', textDecoration: 'none' }} to="/about">About</Link>
-      <Link style={{ color: 'white', textDecoration: 'none' }} to="/services">Services</Link>
-      <Link style={{ color: 'white', textDecoration: 'none' }} to="/contact">Contact</Link>
+      <Link style={{ color: '#fff', textDecoration: 'none' }} to="/">Home</Link>
+      <Link style={{ color: '#fff', textDecoration: 'none' }} to="/about">About</Link>
+      <Link style={{ color: '#fff', textDecoration: 'none' }} to="/services">Services</Link>
+      <Link style={{ color: '#fff', textDecoration: 'none' }} to="/contact">Contact</Link>
     </nav>
   );
 }
