5f822f3c6bb061568e802820abd60f137db16d75
yjoel1 <joelyeddu@gmail.com>
2026-02-02 07:00:51 -0800

    Update

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
