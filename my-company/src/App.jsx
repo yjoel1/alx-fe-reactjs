ad350e80ae2dced18a34613414ab625fd086a8f3
yjoel1 <joelyeddu@gmail.com>
2026-02-02 07:02:05 -0800

    Update

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './Home';
 import About from './About';
 import Services from './Services';
-import Contact from './Contact';
+import Contact from './components/Contact';
 
 function App() {
   return (
